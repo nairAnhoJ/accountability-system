016623898786f2955e72d5dce7772456b14faa51
unknown <malondrasarian@gmail.com>
2025-01-10 17:47:21 +0800

    Theme Toggle - Done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import IconMoon from '../assets/iconMoon'
 import IconSun from '../assets/IconSun'
 
 function Header() {
-  return (
-    <div className='w-screen h-16 bg-white'>
-        <div className='flex items-center justify-between py-3 px-10 h-full'>
-            <h1 className='text-gray-500 font-bold text-xl'>Accountability System</h1>
-            <div className='h-full'>
-                <button className='h-full aspect-square hover:bg-gray-100 rounded-lg flex items-center justify-center'>
-                    <IconSun className='text-gray-500 w-6 h-6'></IconSun>
-                    {/* <IconMoon className='text-gray-500 w-6 h-6'></IconMoon> */}
-                </button>
+    const [dark, setDark] = useState(() => {
+        if(localStorage.getItem('dark') == null){
+            return 'light';
+        }else{
+            return localStorage.getItem('dark');
+        }
+    });
+
+    useEffect(() => {
+        if(dark == 'dark'){
+            document.body.classList.add("dark");
+        }else{
+            document.body.classList.remove("dark");
+        }
+        console.log(dark);
+    }, [dark]);
+
+    const toggleTheme = () => {
+        if(dark == 'dark'){
+            setDark('light');
+            localStorage.setItem('dark', 'light');
+        }else{
+            setDark('dark');
+            localStorage.setItem('dark', 'dark');
+        }
+    };
+
+    return (
+        <div className='w-screen h-16 bg-white dark:bg-gray-800'>
+            <div className='flex items-center justify-between py-3 px-10 h-full'>
+                <h1 className='text-gray-500 font-bold text-xl dark:text-white tracking-wide'>Accountability System</h1>
+                <div className='h-full'>
+                    <button onClick={toggleTheme} className='h-full aspect-square hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg flex items-center justify-center'>
+                        { dark == 'dark' ? <IconSun className='text-gray-400 w-6 h-6'></IconSun> : <IconMoon className='text-gray-500 w-6 h-6'></IconMoon> }
+                    </button>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Header
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,8 +4,8 @@ function Home() {
 
     return (
         <>
-            <div className='bg-gray-50 dark:bg-gray-800 border-t border-gray-200'>
-                <h1>Accountability System</h1>
+            <div className='bg-gray-50 dark:bg-gray-900 h-screen border-t border-gray-200 dark:border-gray-600'>
+                {/* <h1>Accountability System</h1> */}
 
             </div>
         </>
